83a4ff2e5dbfb7766ee8b682606914a11d57959b
weaver010 <abdelrahmannagaty839@gmail.com>
2024-05-29 16:36:49 +0000

    hi

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,21 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  try {
+    const [photoResponse, userResponse] = await Promise.all([
+      uploadPhoto(),
+      createUser(),
+    ]);
+
+    return {
+      photo: photoResponse,
+      user: userResponse,
+    };
+  } catch (error) {
+    console.error('An error occurred:', error);
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
