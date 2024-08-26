457bfbac6b699a8933e6eccf06ed1f468c45bb47
weaver010 <138701111+weaver010@users.noreply.github.com>
2024-08-26 22:11:04 +0300

    Delete 0x06-unittests_in_js directory

--- 0x06-unittests_in_js/4-payment.js
+++
@@ -1,7 +0,0 @@
-const utils = require('./utils');
-
-module.exports = function sendPaymentRequestToApi(totalAmount, totalShipping) {
-  const total = utils.calculateNumber('SUM', totalAmount, totalShipping);
-  console.log(`The total is: ${total}`);
-  return total;
-};
--- 0x06-unittests_in_js/6-payment_token.js
+++
@@ -1,4 +0,0 @@
-module.exports = function getPaymentTokenFromAPI(success) {
-  if (success === true)
-    return Promise.resolve({ data: 'Successful response from the API' });
-};
